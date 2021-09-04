3124a9cd0390da078941f349f0c628d9d3101eb7
Lucas Figueiredo <lukas.liberato14@gmail.com>
2021-09-04 12:11:23 -0300

    Adicionando injeção de dependências

--- src/app/course/course-list.component.ts
+++ src/app/course/course-list.component.ts
@@ -1,3 +1,4 @@
+import { CourseService } from './course.service';
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
 
@@ -9,30 +10,10 @@ export class CourseListComponent implements OnInit {
 
   courses: Course[] = [];
 
+  constructor(private courseService: CourseService){ }
 
 
   ngOnInit(): void {
-      this.courses = [
-        {
-          id: 1,
-          name: 'Angular: Forms',
-          imageUrl: '/assets/images/forms.png',
-          price: 99.9,
-          code: 'XPS-8796',
-          duration: 120,
-          rating: 4.4,
-          releaseDate: 'December, 2, 2019'
-        },
-        {
-          id: 2,
-          name: 'Angular: HTTP',
-          imageUrl: '/assets/images/http.png',
-          price: 50.2,
-          code: 'LLP-2396',
-          duration: 80,
-          rating: 4,
-          releaseDate: 'November, 2, 2020'
-        }
-      ]
+    this.courses = this.courseService.retrieveAll();
   }
 }
